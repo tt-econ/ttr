a53527d5c58a88b231babee70b2142e9b820f63e
Linh T. Tô <github@linh.to>
2020-12-13 14:15:53 -0500

    ADM-124: Allow extra arguments for theme_tt from specs plots

--- R/specs_plots.R
+++ R/specs_plots.R
@@ -10,9 +10,6 @@
 #' @param palette Change the palette.
 #' Default to `"green"` for green-shaded plot.
 #' Change to `"brown"` for brown-shaded plot.
-#' @param style Change the theme style.
-#' Default to `"slide"` for default slide fonts and themed axis title and text.
-#' Change to `"paper"` for default paper fonts and black axis title and text.
 #' @param point_size A numeric scalar indicating the size of the coefficient estimate points.
 #' Default to `NULL` and set according to the number of specs.
 #' @param error_geom A string indicating the type of geom that should be used to
@@ -28,8 +25,6 @@
 #' Default to `NULL` to use `ggplot2` default.
 #' @param coef_ylabel A string specifying the y-axis label on the coefficient panel.
 #'  Default to `"Coefficient estimate"`.
-#' @param xaxis A boolean specifying whether the xaxis line is shown.
-#'  Default to `FALSE`.
 #' @param coef_xlabel A string specifying the x-axis label on the coefficient panel.
 #'  Default to `NULL` for none.
 #' @param label_text_size A numeric scalar indicating how large the text for the
@@ -43,17 +38,18 @@
 #' @param standalone A boolean to indicate whether this is a standalone plot or
 #'  part of a specification plot with a control grid panel.
 #'  Default to `FALSE`.
+#' @param ... Other arguments passed to `ttr::theme_tt()`
 #' @return A `ggplot2` object depicting the coefficient plot
 #' @export
-specs_coef_plot <- function(coef_grid, palette = "green", style = "slide",
+specs_coef_plot <- function(coef_grid, palette = "green",
                             coef_ylabel = "Coefficient estimate", coef_ylim = NULL,
-                            xaxis = FALSE, coef_xlabel = NULL,
+                            coef_xlabel = NULL,
                             label_text_size = NULL,
                             point_size = NULL, error_alpha = NULL,
                             error_geom = NULL, error_width = 0,
                             hline = NULL,
                             pcolors = NULL,
-                            standalone = FALSE) {
+                            standalone = FALSE, ...) {
   ncoefs <- nrow(coef_grid)
   mygray <- shades::saturation(ttcolor(palette), 0.21)[[1]]
   if (is.null(pcolors)) {
@@ -110,7 +106,7 @@ specs_coef_plot <- function(coef_grid, palette = "green", style = "slide",
         )
       }
     } +
-    ttr::theme_tt(palette = palette, style = style, xaxis = xaxis) +
+    ttr::theme_tt(xaxis = FALSE, palette = palette, ...) +
     {
       if (grepl("^\\s*$", coef_ylabel) || is.null(coef_ylabel)) {
         ggplot2::theme(axis.title.y = ggplot2::element_blank())
@@ -159,9 +155,6 @@ specs_coef_plot <- function(coef_grid, palette = "green", style = "slide",
 #' @param palette Change the palette.
 #' Default to `"green"` for green-shaded plot.
 #' Change to `"brown"` for brown-shaded plot.
-#' @param style Change the theme style.
-#' Default to `"slide"` for default slide fonts and themed axis title and text.
-#' Change to `"paper"` for default paper fonts and black axis title and text.
 #' @param control_geom A string indicating the geom that should be used to
 #'  indicate the presence of controls.
 #' Currently supported are `"circle"` and `"rect"`.
@@ -174,21 +167,20 @@ specs_coef_plot <- function(coef_grid, palette = "green", style = "slide",
 #' @param control_text_size A numeric scalar indicating how large the
 #'  control name text should be.
 #' Default to 9.
-#' @param xaxis A boolean specifying whether the xaxis line is shown.
-#'  Default to `FALSE`.
 #' @param trim_top A numeric scalar indicating how close the bottom panel
 #'  (displaying presence of controls) should be to the
 #'  top panel (displaying presence of coefficients).
 #' Useful when dealing with large CIs.
 #' Default to `0`.
+#' @param ... Other arguments passed to `ttr::theme_tt()`
+#'
 #' @return A `ggplot2` object depicting the control grid plot
 #' @export
-specs_control_plot <- function(control_grid, palette = "green", style = "slide",
+specs_control_plot <- function(control_grid, palette = "green",
                                control_geom = "rect",
                                control_spacing = NULL,
                                control_text_size = 9,
-                               xaxis = FALSE,
-                               trim_top = 0) {
+                               trim_top = 0, ...) {
   control_grid$value <- as.factor(control_grid$value)
   control_grid$key <- as.factor(control_grid$key)
   control_grid$y <- -as.numeric(control_grid$key)
@@ -203,7 +195,7 @@ specs_control_plot <- function(control_grid, palette = "green", style = "slide",
   control_plot <- ggplot2::ggplot(control_grid) +
     ggplot2::scale_fill_manual(values = c("#FFFFFF", mygray)) +
     ggplot2::guides(fill = F) +
-    ttr::theme_tt(palette = palette, style = style, ygrid = FALSE, xaxis = xaxis) +
+    ttr::theme_tt(palette = palette, ygrid = FALSE, xaxis = FALSE, ...) +
     ggplot2::theme(
       plot.margin = grid::unit(c(-trim_top, 0, 0, 0), "lines"),
       axis.text.x = ggplot2::element_blank(),
@@ -277,9 +269,6 @@ specs_combine_plots <- function(coef_plot, control_plot, ncoefs, ratio = 2,
 #' @param palette Change the palette.
 #' Default to `"green"` for green-shaded plot.
 #' Change to `"brown"` for brown-shaded plot.
-#' @param style Change the theme style.
-#' Default to `"slide"` for default slide fonts and themed axis title and text.
-#' Change to `"paper"` for default paper fonts and black axis title and text.
 #' @param ratio A numeric scalar indicating the height ratio of the
 #'  coefficient plot relative to the control grid plot.
 #' @param point_size A numeric scalar indicating the size of the coefficient estimate points.
@@ -297,8 +286,6 @@ specs_combine_plots <- function(coef_plot, control_plot, ncoefs, ratio = 2,
 #' Default to `NULL` to use `ggplot2` default.
 #' @param coef_ylabel A string specifying the y-axis label on the coefficient panel.
 #'  Default to `"Coefficient estimate"`.
-#' @param xaxis A boolean specifying whether the xaxis line is shown.
-#'  Default to `FALSE`.
 #' @param coef_xlabel A string specifying the x-axis label on the coefficient panel.
 #'  Default to `NULL` for none.
 #' @param label_text_size A numeric scalar indicating how large the text for the
@@ -323,14 +310,16 @@ specs_combine_plots <- function(coef_plot, control_plot, ncoefs, ratio = 2,
 #'  top panel (displaying presence of coefficients).
 #' Useful when dealing with large CIs.
 #' Default to `-1`.
+#' @param ... Other arguments passed to `ttr::theme_tt()`
+#'
 #' @return A list of 3 `ggplot2` objects, `coef_plot` depicting the coefficient plot,
 #'  `control_plot` depicting the control panel, and `spec_plot` depicting the
 #'  specification plot with both coefficient and control panels
 #' @export
-specs_plot <- function(coefs,
-                       palette = "green", style = "slide", ratio = 2,
+specs_plot <- function(coefs, palette = "green",
+                       ratio = 2,
                        coef_ylabel = "Coefficient estimate", coef_ylim = NULL,
-                       xaxis = FALSE, coef_xlabel = NULL,
+                       coef_xlabel = NULL,
                        label_text_size = NULL,
                        point_size = NULL, error_alpha = NULL,
                        error_geom = NULL, error_width = 0,
@@ -338,7 +327,7 @@ specs_plot <- function(coefs,
                        control_geom = "rect",
                        control_spacing = NULL,
                        control_text_size = 9,
-                       trim_top = 0) {
+                       trim_top = 0, ...) {
   if (!"index" %in% colnames(coefs)) coefs$index <- 1:nrow(coefs)
   if ("p" %in% colnames(coefs)) coefs$p <- NULL
   prior_coef_col <- base::which(colnames(coefs) == "coef") - 1
@@ -357,22 +346,21 @@ specs_plot <- function(coefs,
   coef_grid <- coefs[, c("coef", "error_low", "error_high", "index", "p")]
 
   coef_plot <- ttr::specs_coef_plot(coef_grid,
-    palette = palette, style = style,
+    palette = palette,
     coef_ylabel = coef_ylabel, coef_ylim = coef_ylim,
-    xaxis = xaxis, coef_xlabel = coef_xlabel,
+    coef_xlabel = coef_xlabel,
     label_text_size = label_text_size,
     point_size = point_size, error_alpha = error_alpha,
     error_geom = error_geom, error_width = error_width,
     hline = hline,
-    standalone = FALSE
+    standalone = FALSE, ...
   )
   control_plot <- ttr::specs_control_plot(control_grid,
-    palette = palette, style = style,
+    palette = palette,
     control_geom = control_geom,
     control_spacing = control_spacing,
     control_text_size = control_text_size,
-    xaxis = xaxis,
-    trim_top = trim_top
+    trim_top = trim_top, ...
   )
   specs_plot <- specs_combine_plots(coef_plot, control_plot, nrow(coef_grid),
     ratio = ratio,
